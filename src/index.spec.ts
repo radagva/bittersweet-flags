797179cebff5270a2178580e70a34a9277c512a8
= <angel@wearecapicua.com>
2025-05-21 09:23:01 -0400

    allow to add computed properties

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,4 @@
-import createOptionset from "."
+import createOptionset from ".";
 
 const optionset = createOptionset({
   weekdays: {
@@ -9,212 +9,227 @@ const optionset = createOptionset({
     thu: 1 << 4,
     fri: 1 << 5,
     sat: 1 << 6,
-  }
-})
+    get weekends(): number[] {
+      return [this.sat, this.sun];
+    },
+  },
+});
 
-const weekdays = optionset({ of: 'weekdays' })
-const workdays = weekdays.valueFor(['mon', 'tue', 'wed', 'thu', 'fri'])
+const weekdays = optionset({ of: "weekdays" });
+const workdays = weekdays.valueFor(["mon", "tue", "wed", "thu", "fri"]);
 
-describe('Bitwise operations', () => {
-  it('Can create an optionset for weekdays', () => {
-    expect(weekdays).not.toBe(undefined)
-  })
+describe("Bitwise operations", () => {
+  it("Can create an optionset for weekdays", () => {
+    expect(weekdays).not.toBe(undefined);
+  });
 
   // MARK: Key generation
-  it('Generates one key for each attribute in weekdays', () => {
-    expect(Object.keys(weekdays.keysDict).length).toBe(7)
-  })
+  it("Generates one key for each attribute in weekdays", () => {
+    expect(Object.keys(weekdays.keysDict).length).toBe(8);
+  });
 
-  it('Generates the right key for each attribute in weekdays', () => {
-    const keys = Object.keys(weekdays.keysDict)
-    expect(keys).toContain('sun')
-    expect(keys).toContain('mon')
-    expect(keys).toContain('tue')
-    expect(keys).toContain('wed')
-    expect(keys).toContain('thu')
-    expect(keys).toContain('fri')
-    expect(keys).toContain('sat')
-  })
+  it("Generates the right key for each attribute in weekdays", () => {
+    const keys = Object.keys(weekdays.keysDict);
+    expect(keys).toContain("sun");
+    expect(keys).toContain("mon");
+    expect(keys).toContain("tue");
+    expect(keys).toContain("wed");
+    expect(keys).toContain("thu");
+    expect(keys).toContain("fri");
+    expect(keys).toContain("sat");
+  });
 
-  it('Generates the right value for each key in weekdays', () => {
-    expect(weekdays.valueOf('sun')).toBe(1)
-    expect(weekdays.valueOf('mon')).toBe(2)
-    expect(weekdays.valueOf('tue')).toBe(4)
-    expect(weekdays.valueOf('wed')).toBe(8)
-    expect(weekdays.valueOf('thu')).toBe(16)
-    expect(weekdays.valueOf('fri')).toBe(32)
-    expect(weekdays.valueOf('sat')).toBe(64)
-  })
+  it("Generates the right value for each key in weekdays", () => {
+    expect(weekdays.valueOf("sun")).toBe(1);
+    expect(weekdays.valueOf("mon")).toBe(2);
+    expect(weekdays.valueOf("tue")).toBe(4);
+    expect(weekdays.valueOf("wed")).toBe(8);
+    expect(weekdays.valueOf("thu")).toBe(16);
+    expect(weekdays.valueOf("fri")).toBe(32);
+    expect(weekdays.valueOf("sat")).toBe(64);
+    expect(weekdays.valueOf("weekends")).toBe(
+      weekdays.valueFor(["sun", "sat"]),
+    );
+  });
 
-  it('Generates the right key for each value in weekdays', () => {
+  it("Generates the right key for each value in weekdays", () => {
     for (const key of weekdays.keysArray) {
-      expect(key in weekdays.origin).toBe(true)
+      expect(key in weekdays.origin).toBe(true);
     }
-  })
+  });
 
   // MARK: Basic operations
-  it('Can add a key to a flag', () => {
-    const flags = weekdays.valueFor(['fri', 'mon', 'sun'])
-    const newFlags = weekdays.add(flags, 'sat')
+  it("Can add a key to a flag", () => {
+    const flags = weekdays.valueFor(["fri", "mon", "sun"]);
+    const newFlags = weekdays.add(flags, "sat");
 
-    expect(newFlags).toBe(weekdays.valueFor(['fri', 'mon', 'sun', 'sat']))
-  })
+    expect(newFlags).toBe(weekdays.valueFor(["fri", "mon", "sun", "sat"]));
+  });
 
-  it('Can remove a key from a flag', () => {
-    const flags = weekdays.valueFor(['fri', 'mon', 'sun'])
-    const newFlags = weekdays.remove(flags, 'sun')
+  it("Can remove a key from a flag", () => {
+    const flags = weekdays.valueFor(["fri", "mon", "sun"]);
+    const newFlags = weekdays.remove(flags, "sun");
 
-    expect(newFlags).toBe(weekdays.valueFor(['fri', 'mon']))
-  })
+    expect(newFlags).toBe(weekdays.valueFor(["fri", "mon"]));
+  });
 
-  it('Can toggle a key in a flag', () => {
-    const flags = weekdays.valueFor(['fri', 'mon', 'sun'])
-    const newFlags = weekdays.toggle(flags, 'sun')
+  it("Can toggle a key in a flag", () => {
+    const flags = weekdays.valueFor(["fri", "mon", "sun"]);
+    const newFlags = weekdays.toggle(flags, "sun");
 
-    expect(newFlags).toBe(weekdays.valueFor(['fri', 'mon']))
+    expect(newFlags).toBe(weekdays.valueFor(["fri", "mon"]));
 
-    const toggledBack = weekdays.toggle(newFlags, 'sun')
-    expect(toggledBack).toBe(weekdays.valueFor(['fri', 'mon', 'sun']))
-  })
+    const toggledBack = weekdays.toggle(newFlags, "sun");
+    expect(toggledBack).toBe(weekdays.valueFor(["fri", "mon", "sun"]));
+  });
 
   // MARK: Computations
-  it('Calculates the right flag for a selection of days from Monday to friday', () => {
-    expect(weekdays.valueFor([
-      weekdays.keysDict.mon,
-      weekdays.keysDict.tue,
-      weekdays.keysDict.wed,
-      weekdays.keysDict.thu,
-      weekdays.keysDict.fri,
-    ])).toBe(62)
-  })
+  it("Calculates the right flag for a selection of days from Monday to friday", () => {
+    expect(
+      weekdays.valueFor([
+        weekdays.keysDict.mon,
+        weekdays.keysDict.tue,
+        weekdays.keysDict.wed,
+        weekdays.keysDict.thu,
+        weekdays.keysDict.fri,
+      ]),
+    ).toBe(62);
+  });
 
-  it('Calculates the right flag for a selection of days from Saturday to Sunday', () => {
-    expect(weekdays.valueFor([
-      weekdays.keysDict.sun,
-      weekdays.keysDict.sat,
-    ])).toBe(65)
-  })
+  it("Calculates the right flag for a selection of days from Saturday to Sunday", () => {
+    expect(
+      weekdays.valueFor([weekdays.keysDict.sun, weekdays.keysDict.sat]),
+    ).toBe(65);
+  });
 
   // MARK: Checks
-  it('Verify if a given flag contains one key', () => {
-    const flag = weekdays.valueFor(['fri', 'mon', 'sun'])
+  it("Verify if a given flag contains one key", () => {
+    const flag = weekdays.valueFor(["fri", "mon", "sun"]);
 
-    expect(weekdays.contains(flag, 'fri')).toBe(true)
-    expect(weekdays.contains(flag, 'mon')).toBe(true)
-    expect(weekdays.contains(flag, 'sun')).toBe(true)
-    expect(weekdays.contains(flag, 'wed')).toBe(false)
-    expect(weekdays.contains(flag, 'thu')).toBe(false)
-    expect(weekdays.contains(flag, 'sat')).toBe(false)
-    expect(weekdays.contains(flag, 'tue')).toBe(false)
-  })
+    expect(weekdays.contains(flag, "fri")).toBe(true);
+    expect(weekdays.contains(flag, "mon")).toBe(true);
+    expect(weekdays.contains(flag, "sun")).toBe(true);
+    expect(weekdays.contains(flag, "wed")).toBe(false);
+    expect(weekdays.contains(flag, "thu")).toBe(false);
+    expect(weekdays.contains(flag, "sat")).toBe(false);
+    expect(weekdays.contains(flag, "tue")).toBe(false);
+  });
 
-  it('Verify if a given flag does not contains one key', () => {
-    const flags = weekdays.valueFor(['fri', 'mon', 'sun'])
-    const contains = weekdays.contains(flags, 'sat')
+  it("Verify if a given flag does not contains one key", () => {
+    const flags = weekdays.valueFor(["fri", "mon", "sun"]);
+    const contains = weekdays.contains(flags, "sat");
 
-    expect(contains).toBe(false)
-  })
+    expect(contains).toBe(false);
+  });
 
-  it('Can verify if a given flag contains all the specified keys', () => {
-    expect(weekdays.containsAll(workdays, ['fri', 'tue'])).toBe(true)
-    expect(weekdays.containsAll(workdays, ['fri', 'sat'])).toBe(false)
-  })
+  it("Can verify if a given flag contains all the specified keys", () => {
+    expect(weekdays.containsAll(workdays, ["fri", "tue"])).toBe(true);
+    expect(weekdays.containsAll(workdays, ["fri", "sat"])).toBe(false);
+  });
 
   // MARK: Intersections
-  it('Can intersect two optionsets returning values', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const conflicts = weekdays.intersections(workdays, holydays)
+  it("Can intersect two optionsets returning values", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const conflicts = weekdays.intersections(workdays, holydays);
 
-    expect(conflicts.length).toBe(2)
+    expect(conflicts.length).toBe(2);
 
-    const [thu, fri] = conflicts
+    const [thu, fri] = conflicts;
 
-    expect(thu).toBe(weekdays.valueOf('thu'))
-    expect(fri).toBe(weekdays.valueOf('fri'))
-  })
+    expect(thu).toBe(weekdays.valueOf("thu"));
+    expect(fri).toBe(weekdays.valueOf("fri"));
+  });
 
-  it('Can intersect two optionsets returning keys', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const conflicts = weekdays.intersections(workdays, holydays, 'keys')
+  it("Can intersect two optionsets returning keys", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const conflicts = weekdays.intersections(workdays, holydays, "keys");
 
-    expect(conflicts.length).toBe(2)
+    expect(conflicts.length).toBe(2);
 
-    const [thu, fri] = conflicts
+    const [thu, fri] = conflicts;
 
-    expect(thu).toBe('thu')
-    expect(fri).toBe('fri')
-  })
+    expect(thu).toBe("thu");
+    expect(fri).toBe("fri");
+  });
 
-  it('Can intersect two optionsets returning keys/value pairs', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const conflicts = weekdays.intersections(workdays, holydays, 'key-value-pairs')
+  it("Can intersect two optionsets returning keys/value pairs", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const conflicts = weekdays.intersections(
+      workdays,
+      holydays,
+      "key-value-pairs",
+    );
 
-    expect(conflicts.length).toBe(2)
+    expect(conflicts.length).toBe(2);
 
-    const [[thu, thuv], [fri, friv]] = conflicts
+    const [[thu, thuv], [fri, friv]] = conflicts;
 
-    expect(thu).toBe('thu')
-    expect(thuv).toBe(weekdays.valueOf('thu'))
-    expect(fri).toBe('fri')
-    expect(friv).toBe(weekdays.valueOf('fri'))
-  })
+    expect(thu).toBe("thu");
+    expect(thuv).toBe(weekdays.valueOf("thu"));
+    expect(fri).toBe("fri");
+    expect(friv).toBe(weekdays.valueOf("fri"));
+  });
 
-  it('Can intersect two optionsets returning keys/value object', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const conflicts = weekdays.intersections(workdays, holydays, 'key-value-object')
+  it("Can intersect two optionsets returning keys/value object", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const conflicts = weekdays.intersections(
+      workdays,
+      holydays,
+      "key-value-object",
+    );
 
-    expect(Object.keys(conflicts).length).toBe(2)
+    expect(Object.keys(conflicts).length).toBe(2);
 
-    const { thu, fri, mon, sun, tue, sat, wed } = conflicts
+    const { thu, fri, mon, sun, tue, sat, wed } = conflicts;
 
-    expect(thu).toBe(weekdays.valueOf('thu'))
-    expect(fri).toBe(weekdays.valueOf('fri'))
-    expect(mon).not.toBeDefined()
-    expect(sun).not.toBeDefined()
-    expect(tue).not.toBeDefined()
-    expect(sat).not.toBeDefined()
-    expect(wed).not.toBeDefined()
-  })
+    expect(thu).toBe(weekdays.valueOf("thu"));
+    expect(fri).toBe(weekdays.valueOf("fri"));
+    expect(mon).not.toBeDefined();
+    expect(sun).not.toBeDefined();
+    expect(tue).not.toBeDefined();
+    expect(sat).not.toBeDefined();
+    expect(wed).not.toBeDefined();
+  });
 
   // Mark: Difference
-  it('Can calculate the difference between two optionsets returning values', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const diff = weekdays.difference(workdays, holydays)
-    expect(diff.length).toBe(3)
-    expect(diff).toContain(weekdays.valueOf('mon'))
-    expect(diff).toContain(weekdays.valueOf('tue'))
-    expect(diff).toContain(weekdays.valueOf('wed'))
-  })
+  it("Can calculate the difference between two optionsets returning values", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const diff = weekdays.difference(workdays, holydays);
+    expect(diff.length).toBe(3);
+    expect(diff).toContain(weekdays.valueOf("mon"));
+    expect(diff).toContain(weekdays.valueOf("tue"));
+    expect(diff).toContain(weekdays.valueOf("wed"));
+  });
 
-  it('Can calculate the difference between two optionsets returning keys', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const diff = weekdays.difference(workdays, holydays, 'keys')
-    expect(diff.length).toBe(3)
-    expect(diff).toContain('mon')
-    expect(diff).toContain('tue')
-    expect(diff).toContain('wed')
-  })
+  it("Can calculate the difference between two optionsets returning keys", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const diff = weekdays.difference(workdays, holydays, "keys");
+    expect(diff.length).toBe(3);
+    expect(diff).toContain("mon");
+    expect(diff).toContain("tue");
+    expect(diff).toContain("wed");
+  });
 
-  it('Can calculate the difference between two optionsets returning key/value pairs', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const diff = weekdays.difference(workdays, holydays, 'key-value-pairs')
+  it("Can calculate the difference between two optionsets returning key/value pairs", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const diff = weekdays.difference(workdays, holydays, "key-value-pairs");
 
-    expect(diff.length).toBe(3)
+    expect(diff.length).toBe(3);
 
     for (const [key, value] of diff) {
-      expect(value).toBe(weekdays.valueOf(key))
+      expect(value).toBe(weekdays.valueOf(key));
     }
-  })
+  });
 
-  it('Can calculate the difference between two optionsets returning key/value object', () => {
-    const holydays = weekdays.valueFor(['thu', 'fri'])
-    const diff = weekdays.difference(workdays, holydays, 'key-value-object')
+  it("Can calculate the difference between two optionsets returning key/value object", () => {
+    const holydays = weekdays.valueFor(["thu", "fri"]);
+    const diff = weekdays.difference(workdays, holydays, "key-value-object");
 
-    expect(Object.keys(diff).length).toBe(3)
+    expect(Object.keys(diff).length).toBe(3);
 
     for (const key in diff) {
-      const attrb = key as keyof typeof diff
-      expect(diff[attrb]).toBe(weekdays.valueOf(attrb))
+      const attrb = key as keyof typeof diff;
+      expect(diff[attrb]).toBe(weekdays.valueOf(attrb));
     }
-  })
-})
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,45 @@
-type IntersectionReturnType = 'key-value-pairs' | 'key-value-object' | 'keys' | 'values'
+type IntersectionReturnType =
+  | "key-value-pairs"
+  | "key-value-object"
+  | "keys"
+  | "values";
 
 const freeze = <T extends Record<string, any>>(obj: T) => Object.freeze(obj);
+type OptionSet = Record<string, number | number[]>;
 
-function createOptionset<T extends Record<string, Record<string, number>>>(sets: T) {
+const getValue = (value: number | number[]): number => {
+  if (typeof value === "number") return value;
+
+  return value.reduce((acc, n) => acc | n, 0);
+};
+
+function createOptionset<T extends Record<string, OptionSet>>(sets: T) {
   function factory<
     U extends keyof T,
     V extends keyof T[U],
-    W extends V | number
-  >({
-    of: option
-  }: {
-    of: U;
-  }) {
-
+    W extends V | number,
+  >({ of: option }: { of: U }) {
     const origin = sets[option];
-    const max = Object.values(origin).reduce<number>((acc, value) => acc + value, 0);
+    const max = Object.values(origin).reduce<number>(
+      (acc, value) => acc + getValue(value),
+      0,
+    );
     const keysDict = Object.keys(origin).reduce(
       (acc, key) => ({ ...acc, [key]: key }),
       {} as Record<V, V>,
     );
     const keysArray = Object.keys(origin) as (keyof typeof origin)[];
 
-
     function isAccessingByKey(value: unknown): value is V {
-      return (value as string) in origin
+      return (value as string) in origin;
     }
 
     function translateValue(value: unknown): number {
       if (isAccessingByKey(value)) {
-        return Number(origin[value])
+        return Number(origin[value]);
       }
 
-      return Number(value)
+      return Number(value);
     }
 
     /**
@@ -39,29 +47,29 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
      * @param from
      * @returns number
      */
-    function labelsFor(from: number) {
+    function labelsFor(from: number): V[] {
       return Object.entries(origin)
         .filter(([, bit]) => (from & Number(bit)) !== 0)
         .map(([attribute]) => attribute) as V[];
-    };
+    }
 
     /**
      * @description takes an array of keys for the created instance of optionset
      * and generates a number representing the flag for that combination of keys
-     * @param keys 
+     * @param keys
      * @returns number
      */
-    function valueFor(keys: V[]) {
+    function valueFor(keys: V[]): number {
       return keys.reduce((acc, key) => (acc |= translateValue(key)), 0);
-    };
+    }
 
     /**
      * @description Gets the value for an specific key
      * @param ofKey
      * @returns
      */
-    function valueOf(key: V) {
-      return origin[key]
+    function valueOf(key: V): number {
+      return getValue(origin[key]);
     }
 
     /**
@@ -70,9 +78,9 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
      * @param key
      * @returns number
      */
-    function contains(from: number, key: W) {
+    function contains(from: number, key: W): boolean {
       return (from & translateValue(key)) !== 0;
-    };
+    }
 
     /**
      * @description checks if all the specified keys are included into the `from` flag
@@ -80,20 +88,20 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
      * @param keys
      * @returns
      */
-    function containsAll(from: number, keys: W[]) {
-      return keys.map(translateValue).every(key => (from & key) !== 0)
+    function containsAll(from: number, keys: W[]): boolean {
+      return keys.map(translateValue).every((key) => (from & key) !== 0);
     }
 
     /**
      * @description adds a new value to a given flag.
      * If the value is already in the flag, the return value will not change.
-     * @param from 
-     * @param key 
+     * @param from
+     * @param key
      * @returns number
      */
-    function add(from: number, key: W) {
+    function add(from: number, key: W): number {
       return from | translateValue(key);
-    };
+    }
 
     /**
      * @description Removes a value from a given flag.
@@ -102,9 +110,9 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
      * @param key
      * @returns number
      */
-    function remove(from: number, key: W) {
+    function remove(from: number, key: W): number {
       return from & ~translateValue(key);
-    };
+    }
 
     /**
      * @description adds a new value to the given flag, is the value was already added, it will be removed
@@ -112,114 +120,154 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
      * @param key
      * @returns number
      */
-    function toggle(from: number, key: W) {
-      return from ^= translateValue(key);
-    };
+    function toggle(from: number, key: W): number {
+      return (from ^= translateValue(key));
+    }
 
     /**
      * @description return an array of numbers where the given parameters matches.
      * Each number represents the value of the bit flag
      * @param lhs
      * @param rhs
-     * @returns 
+     * @returns
      */
-    function intersections(lhs: number, rhs: number, returning?: 'values'): number[]
+    function intersections(
+      lhs: number,
+      rhs: number,
+      returning?: "values",
+    ): number[];
     /**
      * @description return an array of key,value pairs where the given parameters matches
      * @param lhs
      * @param rhs
-     * @returns 
+     * @returns
      */
-    function intersections(lhs: number, rhs: number, returning?: 'key-value-pairs'): [V, number][]
+    function intersections(
+      lhs: number,
+      rhs: number,
+      returning?: "key-value-pairs",
+    ): [V, number][];
     /**
      * @description return an object of key values pairs where the given parameters matches.
      * The key represents the label and the value represents the bit flag
      * @param lhs
      * @param rhs
-     * @returns 
+     * @returns
      */
-    function intersections(lhs: number, rhs: number, returning?: 'key-value-object'): Record<V, number>
+    function intersections(
+      lhs: number,
+      rhs: number,
+      returning?: "key-value-object",
+    ): Record<V, number>;
     /**
      * @description return an array of strings where the given parameters matches.
      * Each string represents de label of the bit flag
      * @param lhs
      * @param rhs
-     * @returns 
+     * @returns
      */
-    function intersections(lhs: number, rhs: number, returning?: 'keys'): V[]
-    function intersections(lhs: number, rhs: number, returning: IntersectionReturnType = 'values'): [V, number][] | Record<V, number> | V[] | number[] {
-      const filtered = Object.entries(origin).filter(([_, value]) => {
+    function intersections(lhs: number, rhs: number, returning?: "keys"): V[];
+    function intersections(
+      lhs: number,
+      rhs: number,
+      returning: IntersectionReturnType = "values",
+    ): [V, number][] | Record<V, number> | V[] | number[] {
+      const filtered = Object.entries(origin).filter(([_, _value]) => {
+        const value = getValue(_value);
         return (lhs & value) !== 0 && (rhs & value) !== 0;
-      })
+      }) as [string, number][];
 
       switch (returning) {
-        case 'key-value-pairs':
-          return filtered as [V, number][]
-        case 'key-value-object':
-          return filtered.reduce((acc, [key, value]) => ({
-            ...acc, [key]: value
-          }), {}) as Record<V, number>
-        case 'keys':
-          return filtered.map(([key]) => key as V)
-        case 'values':
-          return filtered.map(([, value]) => value)
+        case "key-value-pairs":
+          return filtered as [V, number][];
+        case "key-value-object":
+          return filtered.reduce(
+            (acc, [key, value]) => ({
+              ...acc,
+              [key]: value,
+            }),
+            {},
+          ) as Record<V, number>;
+        case "keys":
+          return filtered.map(([key]) => key as V);
+        case "values":
+          return filtered.map(([, value]) => value);
       }
-    };
+    }
 
     /**
-      * @description return an array of numbers where the given parameters does not match.
-      * Each number represents the value of the bit flag
-      * @param lhs
-      * @param rhs
-      * @returns 
-      */
-    function difference(lhs: number, rhs: number, returning?: 'values'): number[]
+     * @description return an array of numbers where the given parameters does not match.
+     * Each number represents the value of the bit flag
+     * @param lhs
+     * @param rhs
+     * @returns
+     */
+    function difference(
+      lhs: number,
+      rhs: number,
+      returning?: "values",
+    ): number[];
     /**
-      * @description return an array of strings where the given parameters does not match.
-      * Each string represents de label of the bit flag
-      * @param lhs
-      * @param rhs
-      * @returns 
-      */
-    function difference(lhs: number, rhs: number, returning?: 'keys'): V[]
+     * @description return an array of strings where the given parameters does not match.
+     * Each string represents de label of the bit flag
+     * @param lhs
+     * @param rhs
+     * @returns
+     */
+    function difference(lhs: number, rhs: number, returning?: "keys"): V[];
     /**
-      * @description return an array of key,value pairs where the given parameters does not match
-      * @param lhs
-      * @param rhs
-      * @returns 
-      */
-    function difference(lhs: number, rhs: number, returning?: 'key-value-pairs'): [V, number][]
+     * @description return an array of key,value pairs where the given parameters does not match
+     * @param lhs
+     * @param rhs
+     * @returns
+     */
+    function difference(
+      lhs: number,
+      rhs: number,
+      returning?: "key-value-pairs",
+    ): [V, number][];
     /**
-      * @description return an object of key values pairs where the given parameters does not match.
-      * The key represents the label and the value represents the bit flag
-      * @param lhs
-      * @param rhs
-      * @returns 
-      */
-    function difference(lhs: number, rhs: number, returning?: 'key-value-object'): Record<V, number>
+     * @description return an object of key values pairs where the given parameters does not match.
+     * The key represents the label and the value represents the bit flag
+     * @param lhs
+     * @param rhs
+     * @returns
+     */
+    function difference(
+      lhs: number,
+      rhs: number,
+      returning?: "key-value-object",
+    ): Record<V, number>;
     /**
-      * @description return an array of key,value pairs where the given parameters does not match
-      * @param lhs
-      * @param rhs
-      * @returns 
-      */
-    function difference(lhs: number, rhs: number, returning: IntersectionReturnType = 'values'): number[] | V[] | [V, number][] | Record<V, number> {
-      const rhsLabels = labelsFor(rhs)
-      const lhsLabels = labelsFor(lhs)
-      const diff = lhsLabels.filter(label => !rhsLabels.includes(label))
-
+     * @description return an array of key,value pairs where the given parameters does not match
+     * @param lhs
+     * @param rhs
+     * @returns
+     */
+    function difference(
+      lhs: number,
+      rhs: number,
+      returning: IntersectionReturnType = "values",
+    ): number[] | V[] | [V, number][] | Record<V, number> {
+      const rhsLabels = labelsFor(rhs);
+      const lhsLabels = labelsFor(lhs);
+      const diff = lhsLabels.filter((label) => !rhsLabels.includes(label));
 
       switch (returning) {
-        case 'keys':
-          return diff
-        case 'values':
-          return diff.map(valueOf)
-        case 'key-value-pairs':
-          return diff.map(label => [label, valueOf(label)] as [V, number])
-        case 'key-value-object':
-          return diff.reduce((acc, key) => ({
-            ...acc, [key]: valueOf(key)
-          }), {} as Record<V, number>)
+        case "keys":
+          return diff;
+        case "values":
+          return diff.map(valueOf);
+        case "key-value-pairs":
+          return diff.map((label) => [label, valueOf(label)] as [V, number]);
+        case "key-value-object":
+          return diff.reduce(
+            (acc, key) => ({
+              ...acc,
+              [key]: valueOf(key),
+            }),
+            {} as Record<V, number>,
+          );
       }
     }
 
@@ -237,11 +285,11 @@ function createOptionset<T extends Record<string, Record<string, number>>>(sets:
       remove,
       toggle,
       intersections,
-      difference
+      difference,
     };
-  };
+  }
 
   return factory;
-};
+}
 
-export default createOptionset
+export default createOptionset;
